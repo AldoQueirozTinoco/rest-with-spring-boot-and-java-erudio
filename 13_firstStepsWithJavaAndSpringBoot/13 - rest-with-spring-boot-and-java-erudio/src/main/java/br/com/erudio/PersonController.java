904d2a38410f56efab5b7fff7bb8453593a8fa72
Aldo Queiroz <aldoqueiroztinoco@gmail.com>
2024-07-20 09:57:11 -0300

    Aulas 12 e 13 e mudança de adress para address

---
+++ 13_firstStepsWithJavaAndSpringBoot/13 - rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/PersonController.java
@@ -0,0 +1,72 @@
+package br.com.erudio;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import br.com.erudio.data.vo.v1.PersonVO;
+import br.com.erudio.services.PersonServices;
+
+//A API Rest retorna dados em JSON, XML... Enquanto a Web retorna html, css...
+//@O controle cria um map do Model Object e encontrar uma view equivalente
+//o @restcontroler retorna o objeto e escreve suas informações direto no http
+@RestController// Mistura do @ResponseBody com o @Controller
+@RequestMapping("/api/person/v1")
+public class PersonController {
+	
+	@Autowired //Age em conjunt com @Service para injeção de dependencias
+	private PersonServices service;
+	//private PersonServices service = new PersonServices();
+	
+	@GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE, 
+			MediaType.APPLICATION_XML_VALUE})
+	public List<PersonVO> findAll(){
+		return service.findAll();
+	}
+	
+	@GetMapping(value = "/{id}",
+			produces ={MediaType.APPLICATION_JSON_VALUE, 
+					MediaType.APPLICATION_XML_VALUE})
+	public PersonVO findById(
+			@PathVariable(value="id") Long id){
+		return service.findById(id);
+	}
+	
+	//CRUD Create Read Update Delete
+	@PutMapping(
+			consumes ={MediaType.APPLICATION_JSON_VALUE, 
+					MediaType.APPLICATION_XML_VALUE},
+			produces ={MediaType.APPLICATION_JSON_VALUE, 
+					MediaType.APPLICATION_XML_VALUE})
+	
+	public PersonVO update(@RequestBody PersonVO person){
+		return service.update(person);
+	}
+	@PostMapping(
+			consumes ={MediaType.APPLICATION_JSON_VALUE, 
+					MediaType.APPLICATION_XML_VALUE},
+			produces ={MediaType.APPLICATION_JSON_VALUE, 
+					MediaType.APPLICATION_XML_VALUE})
+	
+	public PersonVO create(@RequestBody PersonVO person){
+		return service.create(person);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> delete(
+			@PathVariable(value="id") Long id){
+		 service.delete(id);
+		 return ResponseEntity.noContent().build();
+	}
+	
+	
+	
+	
+
+	
+}
+
